87e278d209d898a18fe252a1178d17a075ddfa3d
Lissandro <luismexico2006@gmail.com>
2025-09-22 14:46:43 -0700

    se mejoro el gulp file para build en netlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
-
+import plumber from 'gulp-plumber';
+import browserSync from 'browser-sync';
 
 const sass = gulpSass(dartSass);
 
 // funcion que al ejecutarse, crea un documento y compila el css
 //despues ejecuta la funcion saas
-export function css (done) {
-    src ('src/scss/app.scss')
-        .pipe( sass().on('error', sass.logError))
-        .pipe(dest('dist/css'));
+export function css(done) {
+    src('src/scss/app.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream());
     done();
 }
+
 // funcion para observar los cambios de los archivos en tiempo real
 // con el parametro de gulp --watch
-export function dev(){
+export function dev(done) {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
     watch('src/scss/**/*.scss', css);
+    watch('./*.html').on('change', browserSync.reload);
+    watch('main.js').on('change', browserSync.reload);
 
+    done();
 }
